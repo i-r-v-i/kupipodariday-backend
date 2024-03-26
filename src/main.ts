494c878b78513169c7de06ea7bfb2e9d975a81fd
Irina Vinitskaia <kreneva.i@yandex.ru>
2024-03-26 15:56:29 +0300

    feat: добавлен порт, заготовка для users

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(PORT, () => console.log(`Server started  on port ${PORT}`));
 }
 bootstrap();
