13209a367d1580fde1ce5f5c078dc53330e0fdd8
Irina Vinitskaia <kreneva.i@yandex.ru>
2024-03-29 11:40:03 +0300

    feat: сгенерирован каркас API с помощью nest.cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WishesController } from './wishes/wishes.controller';
+import { WishesService } from './wishes/wishes.service';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersController } from './offers/offers.controller';
+import { OffersService } from './offers/offers.service';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+    }),
     UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [WishesController, OffersController],
+  providers: [WishesService, OffersService],
 })
 export class AppModule {}
