fa1c449ff6fec42ca5541d2caf94cc72de3122fa
Irina Vinitskaia <kreneva.i@yandex.ru>
2024-04-15 11:30:00 +0300

    feat: добавлен JwtAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller()
 export class AuthController {
@@ -10,5 +11,11 @@ export class AuthController {
   @Post('signin')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('signup')
+  async signup(@Request() req) {
+    return req.user;
   }
 }
